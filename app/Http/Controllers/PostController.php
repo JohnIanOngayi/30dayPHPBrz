0d5ba82c47c3faa1cb3e33440601decf94d34c26
JohnIanOngayi <ongayijohnian@gmail.com>
2025-07-05 11:18:43 +0300

    feat: Add Post CRUD functionality

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -34,6 +34,11 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'title' => ['required'],
+            'text' => ['required'],
+            'category_id' => ['required']
+        ]);
         Post::create([
             'title' => $request->input('title'),
             'text' => $request->input('text'),
